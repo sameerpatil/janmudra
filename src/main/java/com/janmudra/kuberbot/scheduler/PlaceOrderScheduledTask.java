72722de4106292d9f8e1aefbd53ddddb786acb97
siddharthburle <vickyburle@gmail.com>
2018-06-16 21:08:03 +0530

    Merging conflicts

--- src/main/java/com/janmudra/kuberbot/scheduler/PlaceOrderScheduledTask.java
+++ src/main/java/com/janmudra/kuberbot/scheduler/PlaceOrderScheduledTask.java
@@ -163,6 +163,7 @@ public class PlaceOrderScheduledTask {
 		
 		Double coverQuantity = OrderCalculatorUtil.
 				getQuantityByPercentage(order.getQuantity(), profitPercent);
+		
 		Double coverPrice = OrderCalculatorUtil.
 				getNewPriceWithBrokerage(order.getPrice(), profitPercent);
 		NewOrder coverOrder = new NewOrder(symbol, coverOrderSide, OrderType.LIMIT, 
