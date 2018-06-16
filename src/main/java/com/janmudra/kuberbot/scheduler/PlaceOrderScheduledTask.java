52f129be1194d2a003e3481590c19c7ec93fec81
siddharthburle <vicky_burle@yahoo.com>
2018-06-16 20:40:02 +0530

    Added CSV Implementation

--- src/main/java/com/janmudra/kuberbot/scheduler/PlaceOrderScheduledTask.java
+++ src/main/java/com/janmudra/kuberbot/scheduler/PlaceOrderScheduledTask.java
@@ -1,6 +1,11 @@
 package com.janmudra.kuberbot.scheduler;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -19,6 +24,7 @@ import com.binance.api.client.domain.account.Order;
 import com.binance.api.client.domain.account.request.OrderRequest;
 import com.binance.api.client.domain.market.OrderBook;
 import com.binance.api.client.domain.market.OrderBookEntry;
+import com.janmudra.kuberbot.utils.CSVUtils;
 import com.janmudra.kuberbot.utils.OrderCalculatorUtil;
 
 @Component
@@ -34,7 +40,7 @@ public class PlaceOrderScheduledTask {
 
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
-    @Scheduled(fixedRate = 120000)
+    //@Scheduled(fixedRate = 120000)
     public void executeKuberOrder() {
 //        log.info("The time is now {}", dateFormat.format(new Date()));
 //        TickerStatistics ticker = client.get24HrPriceStatistics(symbol);
@@ -51,6 +57,18 @@ public class PlaceOrderScheduledTask {
     	}
     }
 
+    
+    public String executeEventBasedKuberOrder(String orderType) {
+    	//boolean orderExists = checkOldOpenOrders();
+    	//if(!orderExists) {
+    		return placeEventBasedNewOrder(orderType);
+    	//}else {
+    		//System.out.println("Order exists so cannot place another order");
+    		//log.info("Order exists so cannot place another order");
+    	//}
+    	//return null;
+    }
+    
     /**
      * Places a new order for the dedicate pair of MARKET type. Followed by gives call to coverOrder
      */
@@ -71,14 +89,57 @@ public class PlaceOrderScheduledTask {
 		 * order...!!!
 		**/
 		NewOrder order = new NewOrder(symbol, OrderSide.SELL, OrderType.MARKET, TimeInForce.GTC, quantity,firstAskEntry.getPrice());
-		NewOrderResponse newOrderResponse = client.newOrder(order);
+		//NewOrderResponse newOrderResponse = client.newOrder(order);
 		log.info("Placed Order = " + order.toString());
-		log.info("Order Response = " + newOrderResponse.getOrderId());
+		//log.info("Order Response = " + newOrderResponse.getOrderId());
 		System.out.println("** Placed Order = " + order.toString());
 		log.info("Now calculating and placing the cover-order");
 		placeCoverOrderForThisOrder(order);
 	}
 
+	/**
+     * Places a new order for the dedicate pair of MARKET type. Followed by gives call to coverOrder
+     */
+	private String placeEventBasedNewOrder(String orderType) {
+		OrderBook orderBook = client.getOrderBook(symbol, 10);
+		List<OrderBookEntry> asks = orderBook.getAsks();
+		List<OrderBookEntry> bids = orderBook.getBids();
+		OrderBookEntry firstAskEntry = asks.get(0);
+		OrderBookEntry firstBidEntry = bids.get(0);
+		
+		String quantity = calculateQuantity(firstAskEntry);//<- Need to work on this
+		/** 
+		 * Important in the below line the orderType is set to MARKET 
+		 * because we want to execute the order immediately so it would be easy to calculate and 
+		 * place the cover order immediately.
+		 * We cannot change it to LIMIT type order because we don't have a mechanism yet to
+		 * poll and check if the order got executed or not without which we cannot place the cover
+		 * order...!!!
+		**/
+		//NewOrder order = new NewOrder(symbol, OrderSide.SELL, OrderType.MARKET, TimeInForce.GTC, quantity,firstAskEntry.getPrice());
+		// Persists this order in CSV File
+		String csvFile = "analysis.csv";
+        FileWriter writer;
+		try {
+			writer = new FileWriter(csvFile,true);
+			DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+			Date date = new Date();
+			CSVUtils.writeLine(writer, Arrays.asList(orderType, symbol, OrderSide.SELL.toString(), OrderType.MARKET.toString(), dateFormat.format(date), quantity.toString(), firstAskEntry.getPrice()));
+		    writer.flush();
+		    writer.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		//NewOrderResponse newOrderResponse = client.newOrder(order);
+		//log.info("Placed Order = " + order.toString());
+		//log.info("Order Response = " + newOrderResponse.getOrderId());
+		//System.out.println("** Placed Order = " + order.toString());
+		log.info("Now calculating and placing the cover-order");
+		//placeCoverOrderForThisOrder(order);
+		return orderType;
+	}
 	/**
 	 * This method looks at the order passed in the parameter and place the cover order for that
 	 * If the passed order is of type OrderSide.SELL then the new order is of OrderSide.BUY of
